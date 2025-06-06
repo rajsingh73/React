acbdaee0a3b9dfa8396701fc3ea5e89c582505de
rajsingh73 <raj85singh95@gmail.com>
2025-06-06 12:26:42 +0530

    Reactapp

---
+++ Reactapp/src/App.js
@@ -0,0 +1,10 @@
+import One from './One'
+function App() {
+  return (
+    <>
+    <One />
+    <h1>Hello motherfucker</h1>
+    </>
+  );
+}
+export default App;
---
+++ Reactapp/src/One.js
@@ -0,0 +1,6 @@
+function One(){
+    return(
+        <h2>This is from 01basic thanks to chai aour code</h2>
+    )
+}
+export default One;
