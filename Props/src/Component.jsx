6658829028f4882fcc391a28f5213b890f6eab5d
rajsingh73 <raj85singh95@gmail.com>
2025-06-06 13:39:47 +0530

    Props

--- Props/src/Component.jsx
+++ Props/src/Component.jsx
@@ -1,15 +1,15 @@
-function Comp(promps){
+function Comp(props){
     return (
         <>
             <div className="max-w-xs p-6 ml-3 inline-block rounded-md shadow-md bg-black mt-4">
             <img
-            src={promps.arr.image || "https://cdn.suwalls.com/wallpapers/animals/cute-kitten-37952-2880x1800.jpg"}
+            src={props.arr.image || "https://cdn.suwalls.com/wallpapers/animals/cute-kitten-37952-2880x1800.jpg"}
             alt=""
             className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
             />
             <div className="mt-6 mb-2">
             <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
-                {promps.arr.id || "Lorem"}
+                {props.arr.id || "Lorem"}
             </span>
             <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
             </div>
